9f89745cb6cc96ec1bd6a86326db8a4740e8039a
stekat <stefandietsch@gmail.com>
2020-10-12 07:57:39 +0200

    bereinigt

--- js/site.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    let navigationMenuButton = document.getElementById('navigation-menu-button');
-    let navigationBar = document.getElementById('navigation-bar');
-
-    navigationMenuButton.addEventListener('click', function(){
-        navigationBar.classList.toggle('is-active');
-        navigationMenuButton.classList.toggle('is-active');
-    });
-}
